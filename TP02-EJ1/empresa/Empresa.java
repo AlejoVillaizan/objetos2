a254f64c46a07678c58cc2bd3d77a7def7a7615e
AlejoVillaizan <alejofvillaizan@gmail.com>
2023-03-23 15:33:54 -0300

    ss

--- TP02-EJ1/empresa/Empresa.java
+++ TP02-EJ1/empresa/Empresa.java
@@ -1,5 +1,21 @@
 package empresa;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class Empresa {
+	private String nombre;
+	private Collection<Empleado> empleados; 
+	private Integer cuit;
+	private List<ReciboDeoSueldo> recibos;
 	
+	public 	Float montoTotalSueldosNetos() {
+		return empleados.stream().
+	}
+
+	
+
 }
+
+
